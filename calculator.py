783e374db3b260167e5212dcb8076d60da89e01b
SnezhanaM <65138638+SnezhanaM@users.noreply.github.com>
2020-09-11 10:56:45 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,3 +6,5 @@ def calculate(string):
         return str(eval(string))
     except (SyntaxError, NameError, TypeError):
         return 'Error in expression: "' + string + '"<br>Fix error and try again'
+    except ValueError:
+        return 'Error. Cannot calculate result of expression: "' + string + '"<br>Fix and try again.'
