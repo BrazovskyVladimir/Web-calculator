9fe5edd81a489f25c676ab90def6b9a78d5e695d
SnezhanaM <65138638+SnezhanaM@users.noreply.github.com>
2020-09-11 03:27:49 +0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+from calculator import calculate
+
+
+def create_server():
+    app = Flask(__name__)
+
+    @app.route('/', methods=['GET'])
+    def calc():
+        parameter = request.args.get('expr')
+        if parameter is None:
+            return 'This server is calculator.<br>' \
+                   'To calculate your expression use GET request with parameter expr.'
+        return calculate(parameter)
+    return app
